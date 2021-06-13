44017f8063e89dcce3d1fd90623671ccc9f5be7e
zafnet <mrobm33@gmail.com>
2021-06-12 21:01:00 -0300

    modificacion de launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,7 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar example &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-bspwm &
+done
 
-echo "Polybar launched..."
+echo "Bars launched..."
+
